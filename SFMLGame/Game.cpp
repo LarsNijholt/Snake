d42e0d6318a7df4371742a421ec4deb5b93055bc
Lars Nijholt <61054131+LarsNijholt@users.noreply.github.com>
2022-04-26 11:00:51 +0200

    Upload

--- SFMLGame/Game.cpp
+++ SFMLGame/Game.cpp
@@ -3,45 +3,23 @@
 
 int main()
 {
-	Game GameRef;
-
-	sf::Window window;
-	window.create(sf::VideoMode(2560, 1440), "My Game Window");
-	window.setVerticalSyncEnabled(true); // sets vsync
-	window.setFramerateLimit(60); // sets framerate
+	//create the window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+	sf::Texture texture;
+	if (!texture.loadFromFile("C:/Users/larsn/source/repos/SFMLGame/SFML/Assets/Duck.jpg"))
+		return EXIT_FAILURE;
+	sf::Sprite sprite(texture);
 	while (window.isOpen())
 	{
-		// Check all the window's events that were triggered since the last iteration of the loop.
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			switch (event.type)
-			{
-			case sf::Event::Closed:
-				printf("the window will now close");
-				window.close();
-				break;
-
-			case sf::Event::KeyPressed:
-				break;
-
-			case sf::Event::LostFocus:
-				break;
-
-			default:
-				break;
-			}
+			if (event.type == sf::Event::Closed) window.close();
 		}
 
-		if (event.type == sf::Event::LostFocus)
-		{
-			GameRef.PrintString();
-		}
+		window.clear(sf::Color::Black);
+		window.draw(sprite);
+		window.display();
 	}
 	return 0;
-}
-
-void Game::PrintString()
-{
-	printf("Lost Focus");
 }
