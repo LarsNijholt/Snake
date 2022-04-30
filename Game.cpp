ddd00fc57ce968909cae89a8397739fb81641840
Lars Nijholt <lars.nijholt@gmail.com>
2022-04-30 15:02:29 +0200

    Commit

--- Game.cpp
+++ Game.cpp
@@ -5,24 +5,12 @@
 
 int main()
 {
-	//create the window
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "My window");
-	sf::Texture texture;
-	auto path = std::filesystem::current_path();
-	if (!texture.loadFromFile(path.generic_string() + "/Assets/Duck.jpg"))
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Game Window");
+	sf::Texture ducktexture;
+	if (!ducktexture.loadFromFile("/Assets/Duck.jpg"))
 		return EXIT_FAILURE;
-	sf::Sprite sprite(texture);
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed) window.close();
-		}
-
-		window.clear(sf::Color::Black);
-		window.draw(sprite);
-		window.display();
-	}
+	sf::Sprite Duck(ducktexture);
+	sf::Vector2u size = ducktexture.getSize();
+	//Duck.setOrigin()
 	return 0;
 }
